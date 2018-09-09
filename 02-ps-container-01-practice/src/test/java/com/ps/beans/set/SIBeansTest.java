75eb72328f883a1fce011ad9b59871c6d96194de
Dmitry <euroshow.light.demyanov@gmail.com>
2018-09-09 20:08:18 +0300

    todo4

--- 02-ps-container-01-practice/src/test/java/com/ps/beans/set/SIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/set/SIBeansTest.java
@@ -1,13 +1,15 @@
 package com.ps.beans.set;
 
+import com.ps.beans.SimpleBeanImpl;
+import com.ps.beans.ctr.ComplexBean2Impl;
+import com.ps.sample.SimpleBean;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * Created by iuliana.cosmina on 3/26/16.
@@ -26,5 +28,18 @@ public class SIBeansTest {
         }
 
         //TODO 4. Retrieve beans of types ComplexBean and make sure their dependencies were correctly set.
+
+        ComplexBeanImpl complexBean0 = ctx.getBean("complexBean0", ComplexBeanImpl.class);
+        assertFalse(complexBean0.isComplex());
+        assertEquals(complexBean0.getSimpleBean().getClass(), SimpleBeanImpl.class);
+
+
+        ComplexBeanImpl complexBean1 = (ComplexBeanImpl) ctx.getBean("complexBean1");
+        assertTrue(complexBean1.isComplex());
+        assertEquals(complexBean1.getSimpleBean().getClass(), SimpleBeanImpl.class);
+
+        ComplexBean2Impl complexBean2 = ctx.getBean("complexBean1", ComplexBean2Impl.class);
+        assertEquals(complexBean2.getSimpleBean1().getClass(), SimpleBeanImpl.class);
+        assertEquals(complexBean2.getSimpleBean2().getClass(), SimpleBeanImpl.class);
     }
 }
